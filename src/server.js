86632c5a1b6923b6b9f7604494d3ec246ae8191f
bioroid17 <trace1997@ajou.ac.kr>
2024-01-14 22:44:41 +0900

    INTRODUCTION TO EXPRESS - Servers Recap

--- src/server.js
+++ src/server.js
@@ -4,26 +4,9 @@ const PORT = 4000;
 
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
+const handleHome = () => {
+  console.log("home");
 };
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, you may continue.");
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.send("I love middlewares");
-};
-
-app.use(logger);
-app.use(privateMiddleware);
 
 app.get("/", handleHome);
 

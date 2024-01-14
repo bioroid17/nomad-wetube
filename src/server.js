d84ab84a7bfe670fbe7e9198fddd83709616ac5c
bioroid17 <trace1997@ajou.ac.kr>
2024-01-14 23:33:57 +0900

    INTRODUCTION TO EXPRESS - Controllers Recap

--- src/server.js
+++ src/server.js
@@ -4,11 +4,8 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => {
-  console.log("home");
-};
-
-app.get("/", handleHome);
+const home = (req, res) => res.send("hello");
+app.get("/", home);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);

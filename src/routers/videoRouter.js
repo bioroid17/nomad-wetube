94f3a888092e729e4fb92662f6125a0d9e1c3f67
bioroid17 <trace1997@ajou.ac.kr>
2024-01-23 18:29:02 +0900

    MONGODB AND MONGOOSE - More Practice part 1

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,15 +1,16 @@
 import express from "express";
 import {
   watch,
+  getUpload,
+  postUpload,
   getEdit,
   postEdit,
-  upload,
-  deleteVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.route("/:id(\\d+)").get(watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;

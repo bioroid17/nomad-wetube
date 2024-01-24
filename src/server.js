2f8e2b19063b3e4bc6c06904a9120ec67a550927
bioroid17 <trace1997@ajou.ac.kr>
2024-01-25 01:54:22 +0900

    MONGODB AND MONGOOSE - Our First Query part 1

--- src/server.js
+++ src/server.js
@@ -1,12 +1,9 @@
-import "./db";
-import "./models/Video";
 import express from "express"; // "express"라는 패키지를 express라는 이름으로 import함
 import morgan from "morgan"; // "morgan"으로부터 import만 가능하면 이름은 상관없음
 import globalRouter from "./routers/globalRouter"; // 이름은 상관없음
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
@@ -18,7 +15,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
+export default app;

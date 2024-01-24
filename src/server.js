d7333a448a6f00c6f44cb9667713af0ddbfe3d50
bioroid17 <trace1997@ajou.ac.kr>
2024-01-24 21:45:06 +0900

    MONGODB AND MONGOOSE - Video Model

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import "./db";
+import "./models/Video";
 import express from "express"; // "express"라는 패키지를 express라는 이름으로 import함
 import morgan from "morgan"; // "morgan"으로부터 import만 가능하면 이름은 상관없음
 import globalRouter from "./routers/globalRouter"; // 이름은 상관없음

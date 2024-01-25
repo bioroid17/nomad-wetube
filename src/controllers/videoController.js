21aae4a3e7cdaac89e49ff7aba6f531db505dd77
bioroid17 <trace1997@ajou.ac.kr>
2024-01-25 23:30:29 +0900

    MONGODB AND MONGOOSE - Creating a Video part 2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,9 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  try {
-    const videos = await Video.find({});
-    return res.render("home", { pageTitle: "Home", videos });
-  } catch (error) {
-    return res.render("server-error", { error });
-  }
+  const videos = await Video.find({});
+  console.log(videos);
+  return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
   const { id } = req.params;
@@ -25,9 +22,9 @@ export const postEdit = (req, res) => {
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  const video = new Video({
+  await Video.create({
     title,
     description,
     createdAt: Date.now(),
@@ -37,6 +34,5 @@ export const postUpload = (req, res) => {
       rating: 0,
     },
   });
-  console.log(video);
   return res.redirect("/");
 };

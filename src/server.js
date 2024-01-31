dfda37c46c7a186ce961ebd2c72117a934baf427
bioroid17 <trace1997@ajou.ac.kr>
2024-01-31 18:40:32 +0900

    USER AUTHENTICATION - Create Account part 1

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express"; // "express"라는 패키지를 express라는 이름으로 import함
 import morgan from "morgan"; // "morgan"으로부터 import만 가능하면 이름은 상관없음
-import globalRouter from "./routers/globalRouter"; // 이름은 상관없음
+import rootRouter from "./routers/rootRouter"; // 이름은 상관없음
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
@@ -11,7 +11,7 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
-app.use("/", globalRouter);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 

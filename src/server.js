f308aceac910012f0bb6a476a23d3ccec24e4a27
bioroid17 <trace1997@ajou.ac.kr>
2024-02-02 00:38:03 +0900

    USER AUTHENTICATION - Sessions and Cookies part 1

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express"; // "express"라는 패키지를 express라는 이름으로 import함
 import morgan from "morgan"; // "morgan"으로부터 import만 가능하면 이름은 상관없음
+import session from "express-session";
 import rootRouter from "./routers/rootRouter"; // 이름은 상관없음
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -11,6 +12,22 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);

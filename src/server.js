082860e1fc1dfa55e1086ac5e2eb2329bf581a58
bioroid17 <trace1997@ajou.ac.kr>
2024-02-04 15:33:02 +0900

    USER AUTHENTICATION - MongoStore

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express"; // "express"라는 패키지를 express라는 이름으로 import함
 import morgan from "morgan"; // "morgan"으로부터 import만 가능하면 이름은 상관없음
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter"; // 이름은 상관없음
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -19,6 +20,7 @@ app.use(
     secret: "Hello!",
     resave: true,
     saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
   })
 );
 

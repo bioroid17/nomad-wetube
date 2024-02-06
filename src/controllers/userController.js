d094f3f8af9c39fabd9d7c03fcbb30a5e2da8f99
bioroid17 <trace1997@ajou.ac.kr>
2024-02-06 23:03:47 +0900

    USER AUTHENTICATION - Github Login part 2

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -59,6 +59,19 @@ export const postLogin = async (req, res) => {
   req.session.user = user;
   return res.redirect("/");
 };
+
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: "1b5553aa370f1d9cbdc8",
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
+export const finishGithubLogin = (req, res) => {};
 export const edit = (req, res) => res.send("Edit Uesr");
 export const remove = (req, res) => res.send("Remove User");
 export const logout = (req, res) => res.send("Logout");

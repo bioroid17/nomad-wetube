3b053e34e33e330a2a78f12540bc904c65e53851
bioroid17 <trace1997@ajou.ac.kr>
2024-02-10 04:08:59 +0900

    VIDEO PLAYER - Volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,7 +4,10 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 
-const handlePlayClick = (e) => {
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
+const handlePlayClick = (event) => {
   if (video.paused) {
     video.play();
   } else {
@@ -12,18 +15,28 @@ const handlePlayClick = (e) => {
   }
   playBtn.innerText = video.paused ? "Play" : "Pause";
 };
-const handleMute = (e) => {
+const handleMute = (event) => {
   if (video.muted) {
     video.muted = false;
   } else {
     video.muted = true;
   }
   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-  volumeRange.value = video.muted ? 0 : 0.5;
+  volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
-const handlePause = (e) => (playBtn.innerText = "Play");
-const handlePlay = (e) => (playBtn.innerText = "Pause");
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volume = value;
+};
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeChange);

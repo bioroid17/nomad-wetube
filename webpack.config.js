cf0bbb39cf243922cbb85bcb0fb4f3bb4c63d561
bioroid17 <trace1997@ajou.ac.kr>
2024-02-09 19:45:32 +0900

    WEBPACK - Better Developer Experience

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,11 @@ module.exports = {
   ],
   entry: "./src/client/js/main.js",
   mode: "development",
+  watch: true,
   output: {
     filename: "js/main.js",
     path: path.resolve(__dirname, "assets"),
+    clean: true,
   },
   module: {
     rules: [

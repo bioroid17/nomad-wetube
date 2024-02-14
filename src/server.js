99d3245fa0e81128cafde67abfd2493c787e15ac
bioroid17 <trace1997@ajou.ac.kr>
2024-02-14 14:02:43 +0900

    VIEWS API - Register View Controller

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import rootRouter from "./routers/rootRouter"; // 이름은 상관없음
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 import { localsMiddleware } from "./middlewares";
+import apiRouter from "./routers/apiRouter";
 
 const app = express();
 const logger = morgan("dev");
@@ -30,5 +31,6 @@ app.use("/assets", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
+app.use("/api", apiRouter);
 
 export default app;
